ed4a9ceb6310aefd5076c391d33a6ebbb69f642a
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-03 11:52:35 -0300

    Criando um comando de saída

--- primeiro-programa-java/src/primeiro/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/primeiro/executavel/PrimeiraClasseJava.java
@@ -2,6 +2,7 @@ package primeiro.executavel;
 
 public class PrimeiraClasseJava {
 	public static void main(String[] args) {
-		System.out.println("Digite main e depois Crtl + Sapce para criar o método main.");
+		System.out.println("Digite main e depois Crtl + Space para criar o método main.");
+		System.out.println("Digite sysout e depois Crtl + Space para cria um comando de saída!");
 	}
 }
