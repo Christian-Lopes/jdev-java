7360c341dd12fe39dae7438708a78024319399fa
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-03 11:55:27 -0300

    Comando git add

--- primeiro-programa-java/src/primeiro/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/primeiro/executavel/PrimeiraClasseJava.java
@@ -4,5 +4,6 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		System.out.println("Digite main e depois Crtl + Space para criar o método main.");
 		System.out.println("Digite sysout e depois Crtl + Space para cria um comando de saída!");
+		System.out.println("Comando git add .: Para adicionar arquivo no repositório local.");
 	}
 }
