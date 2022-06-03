4dc4f9260b033fb9f40fe5d6869b3db4c6f29387
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-03 12:23:03 -0300

    Revert "teste revert"
    This reverts commit 9e43cae9f37d83b49716836745fe1bd80aea0f5f.

--- primeiro-programa-java/src/primeiro/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/primeiro/executavel/PrimeiraClasseJava.java
@@ -6,6 +6,5 @@ public class PrimeiraClasseJava {
 		System.out.println("Digite sysout e depois Crtl + Space para cria um comando de saída!");
 		System.out.println("Comando git add .: Prepara arquivos que serão comitado no repositório local.");
 		System.out.println("Comando git revert: Reverte o arquivo para o ultimo commit.");
-		System.out.println("testando revert!");
 	}
 }
