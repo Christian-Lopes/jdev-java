25680ef32cdc4c6c2231c6a851fcbf598de08ea5
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-03 11:03:49 -0300

    Meu primeiro arquivo no treinamento jdev-java

---
+++ primeiro-programa-java/src/primeiro/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,7 @@
+package primeiro.executavel;
+
+public class PrimeiraClasseJava {
+	public static void main(String[] args) {
+		System.out.println("Digite main e depois Crtl + Sapce para criar o método main.");
+	}
+}
