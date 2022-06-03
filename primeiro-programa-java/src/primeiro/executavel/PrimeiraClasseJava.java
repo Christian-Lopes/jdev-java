56a9f67c7ba2c07b98c2c5564e60db66d73c32c2
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-03 12:14:11 -0300

    atualizando

--- primeiro-programa-java/src/primeiro/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/primeiro/executavel/PrimeiraClasseJava.java
@@ -4,6 +4,7 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		System.out.println("Digite main e depois Crtl + Space para criar o método main.");
 		System.out.println("Digite sysout e depois Crtl + Space para cria um comando de saída!");
-		System.out.println("Comando git add .: Para adicionar arquivo no repositório local.");
+		System.out.println("Comando git add .: Prepara arquivos que serão comitado no repositório local.");
+		System.out.println("Comando git revert: Reverte o arquivo para o ultimo commit.");
 	}
 }
